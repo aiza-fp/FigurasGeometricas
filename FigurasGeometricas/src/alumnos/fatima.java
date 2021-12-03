13d3e30271096811545407b767a05b6a662cf5bd
Fatima-Azza <f_azza@fpzornotza.com>
2021-12-03 11:02:40 +0100

    fatima

--- FigurasGeometricas/src/alumnos/fatima.java
+++ FigurasGeometricas/src/alumnos/fatima.java
@@ -3,10 +3,11 @@ package alumnos;
 public class fatima {
 
 	public fatima() {
-		
+		// TODO Auto-generated constructor stub
 	}
 
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 		System.out.print("fatima");
 
 	}
