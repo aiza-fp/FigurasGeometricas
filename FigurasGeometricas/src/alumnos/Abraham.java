2fa7f19abb685d40ae1374d07e74c52079120b19
ikaslea <ikaslea@DESKTOP-IMN54MA>
2021-12-03 11:21:32 +0100

    hola

---
+++ FigurasGeometricas/src/alumnos/Abraham.java
@@ -0,0 +1,14 @@
+package alumnos;
+
+public class Abraham {
+
+	public Abraham() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
