cd3080183ddacf08bbaaf869a5c53720fe6ecb89
Ikaslea <p_otxoa@fpzornotza.com>
2021-12-03 11:25:30 +0100

    ._.

--- FigurasGeometricas/src/alumnos/Peio.java
+++ FigurasGeometricas/src/alumnos/Peio.java
@@ -5,6 +5,16 @@ public class Peio {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+		/*
+		que miras
+		
+		a copiar a otro lado
+		
+		puto
+		
+		*/
+		
 	}
 
 }
