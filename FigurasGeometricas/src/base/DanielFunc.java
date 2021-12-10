2cc394ca9f953ec946369060d7b4fb8e132b7fad
d_ekomo <d_ekomo@fpzprnotza.com>
2021-12-10 10:57:42 +0100

    funcionalidad Daniel

--- FigurasGeometricas/src/base/DanielFunc.java
+++ FigurasGeometricas/src/base/DanielFunc.java
@@ -3,7 +3,8 @@ package base;
 public class DanielFunc {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//comentario añadido en la rama funcionlaidad Daniel
+
 
 	}
 
