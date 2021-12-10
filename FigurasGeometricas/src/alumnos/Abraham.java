7a1a95fa45814ed5b4ac06159420fb51656f7848
ikaslea <ikaslea@DESKTOP-IMN54MA>
2021-12-10 10:57:23 +0100

    funcionalidadAbraham

--- FigurasGeometricas/src/alumnos/Abraham.java
+++ FigurasGeometricas/src/alumnos/Abraham.java
@@ -1,14 +1,28 @@
 package alumnos;
 
+import figuras.Circulo;
+
 public class Abraham {
 
 	public Abraham() {
 		// TODO Auto-generated constructor stub
 	}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-//hola buenas tardes   
+//hola buenas tardes  
+//comentario añadido en rama funcionalidadAbraham :3
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			Circulo miCirculo = new Circulo();
+			Circulo miCirculo2 = new Circulo(1.0);
+			
+			System.out.println(miCirculo.toString());
+			System.out.println(miCirculo2.toString());
+			
+			if (miCirculo.equals(miCirculo2)) {
+				System.out.println("Son iguales");
+			}else {
+				System.out.println("Son distintos");
+			}
 	}
  
 }
